cd2138618aa3e8f8eb2c5116d6ce5e8ef587e1e6
David Antognoli <david.antognoli@gmail.com>
2019-06-06 20:26:36 -0500

    functional fps controller based on cinemachine and BotD sample game

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material blueMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        // change color to blue
+        meshRenderer.material.color = Color.blue;
+
+        // get color
+        meshRenderer.material.GetColor("_Color");
+        // set color
+        meshRenderer.material.SetColor("_Color", Color.blue);
+
+        Debug.Log(meshRenderer.material.name);
+        Debug.Log(meshRenderer.material.shader.name);
+
+        meshRenderer.material = blueMaterial;
+
+        Material material = new Material(blueMaterial);
+        Debug.Log(material.GetColor(0));
+        meshRenderer.material = material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MaterialColorExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorExample : MonoBehaviour
+{
+    // Fade the color from red to green
+    // back and forth over the defined duration
+
+    Color colorStart = Color.red;
+    Color colorEnd = Color.green;
+    float duration = 1.0f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+    }
+}
