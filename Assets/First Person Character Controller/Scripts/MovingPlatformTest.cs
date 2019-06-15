cb0b4d2a9094698a2cb4059548ea15229eb5f189
David Antognoli <david.antognoli@gmail.com>
2019-06-15 13:27:39 -0500

    support for pushing rigid bodies

---
+++ Assets/First Person Character Controller/Scripts/MovingPlatformTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovingPlatformTest : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+
+    [SerializeField]
+    private float maxMoveDistance = 10.0f;
+
+    private Vector3 startingPosition;
+    new Rigidbody rigidbody;
+    private bool movingForward = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (movingForward)
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed);
+            if (rigidbody.position.z > startingPosition.z + maxMoveDistance)
+            {
+                movingForward = false;
+            }
+        }
+        else
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector3.forward * -speed);
+            if (rigidbody.position.z < startingPosition.z)
+            {
+                movingForward = true;
+            }
+        }
+    }
+}
---
+++ Assets/First Person Character Controller/Scripts/TriggerVolumeTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVolumeTest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player has entered the trigger.");
+        }
+    }
+}
